6447632e93b9d02ab06fd8c111046ccde5c49377
Divya <divyabaheti101@gmail.com>
2025-03-30 18:17:06 +0530

    get tokenURI

--- contracts/SuperMarioWorld.sol
+++ contracts/SuperMarioWorld.sol
@@ -7,11 +7,16 @@ contract SuperMarioWorld is ERC721{
     string public name;
     string public symbol;
     uint256 public tokenCount;
+    mapping (uint256 => string) private _tokeURIs;
 
     constructor(string memory _name, string memory _symbol){
         name = _name;
         symbol = _symbol;
     }
 
-    
+    // Returns a url that points to the metadata
+    function tokenURI(uint256 tokenId) public view returns (string memory){
+        require(_owners[tokenId] != address(0), "Token Id doesn't exist");
+        return _tokeURIs[tokenId];
+    }
 }
