d589d9a2b56210a739e62de970c5ee664ba2a07f
Divya <divyabaheti101@gmail.com>
2025-04-01 20:07:05 +0530

    define mumbai network in hardhat & .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
