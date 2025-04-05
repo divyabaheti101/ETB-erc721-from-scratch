6bc1812799a249a655d852492666933b40a896d1
Divya <divyabaheti101@gmail.com>
2025-04-05 17:29:57 +0530

    mint yoshi from 1155 contract

--- ignition/modules/SuperMarioWorld.js
+++ ignition/modules/SuperMarioWorld.js
@@ -3,13 +3,14 @@
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-module.exports = buildModule("SuperMarioWorldOZModule", (m) => {
+module.exports = buildModule("SuperMarioWorld1155Module", (m) => {
 
-  const SuperMarioWorldOZ = m.contract("SuperMarioWorldOZ", ["SuperMarioWorldOZ", "SPRMOZ"]);
+  const SuperMarioWorld1155 = m.contract("SuperMarioWorld1155", ["SuperMarioWorld1155", "SM1155"]);
   
   //cid taken from pinata for the json or metadat of the nft.
   //m.call(SuperMarioWorldOZ, "mint", ["https://ipfs.io/ipfs/bafkreifylxi6tb3dgezitryc222hmxh6cp5zoandvmcgqdj5bffsv3wa5u"]);
-  m.call(SuperMarioWorldOZ, "mint", ["https://ipfs.io/ipfs/bafkreiebeggiozca5yugbvjaytwozyd3vn7y3pulusjuve2lkbbyklbgum"]);
+  //m.call(SuperMarioWorld1155, "mint", ["https://ipfs.io/ipfs/bafkreiebeggiozca5yugbvjaytwozyd3vn7y3pulusjuve2lkbbyklbgum"]);
+  m.call(SuperMarioWorld1155, "mint", [10, "https://ipfs.io/ipfs/bafkreihc3dw4vidteyaxbznk7ous7l6brrecs2s6xz7cu3lfvz322uuwn4"]);
 
-  return { SuperMarioWorldOZ };
+  return { SuperMarioWorld1155 };
 });
