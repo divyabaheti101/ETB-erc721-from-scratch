9baf375b7d22c029dd154c25dbf4e6b7525e2564
Divya <divyabaheti101@gmail.com>
2025-04-06 12:38:11 +0530

    displaying the nfts in grid

--- frontend/nft-visualiser/src/App.js
+++ frontend/nft-visualiser/src/App.js
@@ -6,17 +6,52 @@ function App() {
 	{name: "Mario", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
 	{name: "Luigi", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
 	{name: "Yoshi", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
+	{name: "Donkey Kong", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
+	{name: "Mario", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
+	{name: "Luigi", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
+	{name: "Yoshi", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"},
 	{name: "Donkey Kong", symbol: "SMWC", copies: 10, image: "https://ipfs.io/ipfs/bafybeiavwldch7gi535jr7hkmyz4v3afmsgemy3yokf4eq55hdbjwgqyxu"}
+  
   ]
   return (
     <div className="App">
-		{
-			nfts.map((nft, i) => 
-				<NFTCard nft = {nft} key={i}/>
-			)
-		}
+		<Container>
+			<Title></Title>
+			<Subtitle></Subtitle>
+			<Grid>
+			{
+				nfts.map((nft, i) => 
+					<NFTCard nft = {nft} key={i}/>
+				)
+			}
+			</Grid>
+		</Container>
     </div>
   );
 }
+
+const Title = styled.h1`
+	margin: 0;
+	text-align: center;
+`
+
+const Subtitle = styled.h4`
+	color: gray;
+	margin-top: 0;
+	text-align: center;
+`
+
+const Container = styled.div`
+	width: 70%;
+	max-width: 1200px;
+	margin: auto;
+	margin-top: 100px;
+`
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	row-gap: 40px;
+`
 
 export default App;
