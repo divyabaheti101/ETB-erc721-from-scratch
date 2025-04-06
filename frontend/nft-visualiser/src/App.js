92f1a6f4efa67a22e47a3f947c0226b44afa5bff
Divya <divyabaheti101@gmail.com>
2025-04-06 13:54:28 +0530

    modal attributes

--- frontend/nft-visualiser/src/App.js
+++ frontend/nft-visualiser/src/App.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { NFTCard, NftPhoto } from './components/NFTCard';
 import { useState } from 'react';
+import { NFTModal } from './components/NFTModal';
 
 function App() {
 
@@ -46,79 +47,6 @@ function App() {
     </div>
   );
 }
-
-const NFTModal = (props) => {
-	let nft = props.nft;
-	return (
-		<Modal>
-			<ModalContent>
-				<ModalGrid>
-					<NftPhoto style={{backgroundImage: `url(${nft && nft.image})`, height: 400, width: 400}} />
-					<div>
-						<ModalTitle>{nft.name}</ModalTitle>
-						<Paragraph>{`You own ${nft.copies} copies`}</Paragraph>
-						<SectionText>Description</SectionText>
-						<Paragraph style={{width: 400}}>{nft.description}</Paragraph>
-						<SectionText>Attributes</SectionText>
-					</div>
-				</ModalGrid>
-				<CloseButton onClick={() => props.toggleModal()}>
-					&times;
-				</CloseButton>
-			</ModalContent>
-		</Modal>
-	)
-}
-
-
-const CloseButton = styled.span`
-	position: absolute;
-	right: 0;
-	top: 0;
-	padding: 20px 25px 0 0;
-	font-size: 20px;
-	font-weight: bold;
-	cursor: pointer;
-`
-const ModalTitle = styled.h1`
-	margin: 0;
-`
-
-const Paragraph = styled.p`
-	margin: 0 0 15px 0;
-`
-
-const SectionText = styled.h3`
-	margin: 5px 0 5px 0;
-`
-
-const ModalGrid = styled.div`
-	display: inline-grid;
-	grid-template-columns: 1fr 1fr;
-	grid-gap: 40px;
-`
-
-const Modal = styled.div`
-	position: fixed;
-	display: flex;
-	align-items: center;
-	z-index: 100% //stays on top of everything
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	overflow: auto; // enable scroll if needed
-	background-color: rgba(0,0,0,0.5);
-`
-
-const ModalContent = styled.div`
-	position: relative;
-	width: 900px;
-	margin: auto;
-	background-color: white;
-	border-radius: 20px;
-	padding: 20px;
-`
 
 const Title = styled.h1`
 	margin: 0;
